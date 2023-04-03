1a88379d05cf5cc4878eece7665236e463576bbb
Ollivier Robert <roberto@keltia.net>
2023-04-03 14:58:59 +0200

    Use filter().

--- src/main.rs
+++ src/main.rs
@@ -79,34 +79,34 @@ fn main() -> Result<()> {
 
     // Now look into every table header and table in parallel
     //
-    hdrs.iter().zip(doc.select(&sel)).for_each(|(n, e)| {
+    hdrs.iter().zip(doc.select(&sel)).for_each(|(name, e)| {
         // For each line
         //
-        debug!("frag={:?}", e.html());
+        info!("Table {name}");
 
-        info!("Table");
+        debug!("frag={}", e.html());
 
         // Now we want each <tr>
         //
         let sel = Selector::parse("tr").unwrap();
         let iter = e.select(&sel);
 
-        let mut area = Area::new(n);
+        let mut area = Area::new(name);
 
-        iter.for_each(|e| {
-            debug!("td={e:?}");
-            let frag = e.html();
+        iter
+            .filter(|e| !e.html().contains("SAC"))
+            .for_each(|e| {
+                debug!("td={e:?}");
+                let frag = e.html();
 
-            // Filter
-            //
-            let frag = re.replace_all(&frag, "");
+                // Filter
+                //
+                let frag = re.replace_all(&frag, "");
 
-            // Get what we want
-            //
-            let (_, (a, b)) = parse_tr(&frag).unwrap();
-            if !a.contains("SAC") {
+                // Get what we want
+                //
+                let (_, (a, b)) = parse_tr(&frag).unwrap();
                 area.add(a, b);
-            }
         });
         println!("area={}\n", area);
     });
