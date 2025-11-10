b89f0ee7c86ac2d7bf3ac5f38e1eb1a6139848be
Ollivier Robert <roberto@keltia.net>
2025-11-10 14:41:28 +0100

    Fix tests with assert_cmd v2.1+.

--- tests/cli-opts.rs
+++ tests/cli-opts.rs
@@ -1,42 +1,44 @@
 use std::fs;
 
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin;
+use assert_cmd::prelude::*;
+use std::process::Command;
 
 const BIN: &str = "fetch-sac";
 
 #[test]
 fn test_empty_args() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.assert().success();
 }
 
 #[test]
 fn test_help() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.arg("-h").assert().success();
 }
 
 #[test]
 fn test_version() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.arg("-V").assert().success();
 }
 
 #[test]
 fn test_json() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.arg("-J").assert().success();
 }
 
 #[test]
 fn test_csv() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.arg("-C").assert().success();
 }
 
 #[test]
 fn test_output_file() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(cargo_bin!());
     cmd.arg("-o").arg("foo.txt").assert().success();
     fs::remove_file("foo.txt").unwrap()
 }
